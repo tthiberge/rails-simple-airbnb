93b98ebd5880dc9f756dbb189db1371e6caa6f58
Theophile Thiberge <tthiberge@gmail.com>
2023-02-22 17:02:12 +0100

    starting airbnb simple

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,41 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests )
+  end
+end
